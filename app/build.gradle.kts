11f04e8228d842f01704ef8dff4fc2151bf1dfdb
Naitik <iib2023036@iiita.ac.in>
2023-11-04 05:49:37 +0530

    commit 5 natik home_fragment , p1,p2,p3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,17 +40,21 @@ android {
 }
 
 dependencies {
-
+//    classpath("com.google.gms:google-services:4.3.10")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.2.0")
+//    implementation(project(mapOf("path" to ":app")))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.github.yuyakaido:CardStackView:v2.3.4")
     implementation ("androidx.appcompat:appcompat:1.3.1")
     implementation ("androidx.drawerlayout:drawerlayout:1.1.1")
+    implementation("com.google.firebase:firebase-database:20.0.0")
+    implementation("com.google.firebase:firebase-core:20.0.0")
+
 
 }
